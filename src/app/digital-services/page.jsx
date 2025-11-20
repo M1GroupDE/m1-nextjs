14b79a36c62aa395c9665e98f11c4360493bd3ce
Ali Nasiri <alinasiri8102@gmail.com>
2025-11-20 23:57:20 +0330

    add dg

---
+++ src/app/digital-services/page.jsx
@@ -0,0 +1,65 @@
+import Hero from "@/components/Hero";
+import IconCard from "@/components/IconCard";
+import InfoCard from "@/components/InfoCard";
+import PageIntro from "@/components/PageIntro";
+import Steps from "@/components/Steps";
+
+const iconCardData = [
+  { icon: "/img/Vorteile.png", title: "Vorteile" },
+  {
+    icon: "/img/Lernen überall & jederzeit.png",
+    title: "Lernen überall & jederzeit",
+  },
+  {
+    icon: "/img/Nachhaltig & papierlos.png",
+    title: "Nachhaltig & papierlos",
+  },
+  {
+    icon: "/img/Volle Kontrolle über deinen Fortschritt.png",
+    title: "Volle Kontrolle über deinen Fortschritt",
+  },
+];
+
+const infoCardData = [
+  {
+    title: "Online-Theorieunterricht",
+    description: "Theorie bequem von zu Hause – live oder als Aufzeichnung.",
+  },
+  {
+    title: "360° Fahrsimulator",
+    description: "Realistisches Fahrgefühl ohne Risiko – perfekt zum Einstieg.",
+  },
+  {
+    title: "M1Plus App",
+    description: "Lernfortschritt, Termine & Rechnungen – alles in einer App.",
+  },
+  {
+    title: "Digitale Lernplattform",
+    description: "Zugriff auf Tests, Lernvideos und Prüfungsfragen rund um die Uhr",
+  },
+];
+
+const page = () => {
+  return (
+    <main id="class-page" className="digital-services">
+      <Hero img={"/img/dg-hero.png"} />
+
+      <PageIntro
+        title={"Digitale Services für deine Führerscheinausbildung"}
+        description={
+          "Lerne flexibel, modern und effizient – mit unseren digitalen Tools, Online-Unterricht und 360°-Simulator."
+        }
+        cta={"JETZT ANMELDEN"}
+        link={"#"}
+      />
+
+      <IconCard title={"Vorteile"} data={iconCardData} />
+
+      <Steps img={"/img/dg-steps.png"} />
+
+      <InfoCard title={"Unsere digitalen Angebote"} data={infoCardData} />
+    </main>
+  );
+};
+
+export default page;
